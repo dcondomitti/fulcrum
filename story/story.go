ca991c40f2c1a989f02090d54905e2c217938a03
Dan Condomitti <dan@interval.io>
2015-06-25 19:33:13 -0400

    adsf

--- story/story.go
+++ story/story.go
@@ -1,5 +1,15 @@
 package story
 
 type Story struct {
-	Title string `json:"title"`
+	Title    string
+	Type     string
+	State    string
+	Estimate int
+}
+
+type SlackFormat struct {
+	Attachments []SlackMessage `json:"attachments"`
+}
+
+type SlackMessage struct {
 }
